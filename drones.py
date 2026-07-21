c7ad565d81e91478c2cb515e4b24b86fa30872a7
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-07-21 16:25:56 +0200

    Update drones.py

--- drones.py
+++ drones.py
@@ -1,6 +1,7 @@
 class Drone:
 
-    def __init__(self, drone_id: int, start: str, goal: str):
+    def __init__(self, drone_id: int, start: str, goal: str) -> None:
+        """Creates drones and stablishes the end and start for the path"""
 
         self.id: int = drone_id
         self.position: str = start
