c206e1552e2f7207496d5bd2bf497e9b81c04faf
Joel H. <166711728+Artiehen@users.noreply.github.com>
2026-07-21 16:34:53 +0200

    Update graph.py

--- graph.py
+++ graph.py
@@ -3,17 +3,19 @@ from typing import DefaultDict
 
 
 # neighbor -> edge capacity
-class Zone:
-    def __init__(self, name: str, cost: int = 1, max_drones: int = 1):
-        self.name: str = name
-        self.cost: int = cost
-        self.max_drones: int = max_drones
-        self.neighbors: dict[str, int] = {}
+# class Zone:
+#     def __init__(self, name: str, cost: int = 1, max_drones: int = 1):
+#         """Initializes"""
+#         self.name: str = name
+#         self.cost: int = cost
+#         self.max_drones: int = max_drones
+#         self.neighbors: dict[str, int] = {}
 
 
 class Graph:
 
     def __init__(self, hubs: dict, connections: list):
+        """Maps out the graph's hubs and connections"""
 
         self.hubs = hubs
 
